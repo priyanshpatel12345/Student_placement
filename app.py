6c2c233572b4d889e5f1b370845e85956c339511
Priyansh Patel <priyanshpatel741@gmail.com>
2025-07-31 09:38:38 +0530

    upgrade app.py

--- app.py
+++ app.py
@@ -64,5 +64,9 @@ def failure():
         else:
             return redirect(url_for("prediction"))
 
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render gives PORT as env variable
+    app.run(host='0.0.0.0', port=port)
